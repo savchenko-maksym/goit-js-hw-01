2b50b996f55ff05944d545370871da52e615de44
savchenko-maksym <maxim.savchenko91@gmail.com>
2024-11-30 23:05:17 +0200

    add js

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+"use strict";
+function getElementWidth(content, padding, border) {
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding) * 2;
+  const borderWidth = parseFloat(border) * 2;
+  const totalWidth = contentWidth + paddingWidth + borderWidth;
+  return `Total Width Element = ${totalWidth}`;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
